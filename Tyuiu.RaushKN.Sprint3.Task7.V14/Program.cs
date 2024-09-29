57cf9bd8bc89cda827eaba47670a58429b96fde1
Кирилл Рауш <Tyuiu.RaushKN@outlook.com>
2024-09-29 16:06:45 +0500

    Sprint3

---
+++ Tyuiu.RaushKN.Sprint3.Task7.V14/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.RaushKN.Sprint3.Task7.V14.Lib;
+
+namespace Tyuiu.RaushKN.Sprint3.Task7.V14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Рауш К. Н. | АСОиУб-24-1";
+            Console.WriteLine("###########################################################################");
+            Console.WriteLine("# Спринт #3                                                               #");
+            Console.WriteLine("# Тема: Добавление к решению итоговых проектов по спринту                 #");
+            Console.WriteLine("# Задание #7                                                              #");
+            Console.WriteLine("# Вариант #14                                                            #");
+            Console.WriteLine("# Выполнил Рауш Кирилл Николавевич | АСОиУб-24-1                          #");
+            Console.WriteLine("###########################################################################");
+            Console.WriteLine("# УСЛОВИЕ:                                                                #");
+            Console.WriteLine("# Написать программу, которая выводит таблицу значений функции            #");
+            Console.WriteLine("#                                                                         #");
+            Console.WriteLine("#                                                                         #");
+            Console.WriteLine("###########################################################################");
+            Console.WriteLine("# ИСХОДНЫЕ ДАННЫЕ:                                                        #");
+            Console.WriteLine("###########################################################################");
+
+            int startValue = -5;
+            int stopValue = 5;
+            double[] res = ds.GetMassFunction(startValue, stopValue);
+
+            Console.WriteLine("Начало числового отрезка = " + startValue);
+            Console.WriteLine("Конец числового отрезка = " + stopValue);
+
+
+            Console.WriteLine("###########################################################################");
+            Console.WriteLine("# РЕЗУЛЬТАТ:                                                              #");
+            Console.WriteLine("###########################################################################");
+
+            
+            for (int z = 0; z <=10; z++)
+                Console.WriteLine(res[z]);
+            Console.ReadKey();
+        }
+    }
+}
